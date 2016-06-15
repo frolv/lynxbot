f9e82b42924b3072e6990798de66fd8c2470839c
Alexei Frolov <alexeifrolov3@gmail.com>
2016-06-15 02:08:44 -0400

    Changed to hex literals

--- src/permissions.h
+++ src/permissions.h
@@ -1,19 +1,20 @@
-#define _SUB 1
-#define _MOD 2
-#define _OWN 4
+#define _REG 0x0
+#define _SUB 0x1
+#define _MOD 0x2
+#define _OWN 0x4
 
 typedef unsigned char perm_t;
 
-#define P_RESET(p) ((p) = 0)
+#define P_RESET(p) ((p) = _REG)
 
 #define P_STSUB(p) ((p) |= _SUB)
 #define P_STMOD(p) ((p) |= _MOD)
 #define P_STOWN(p) ((p) |= _OWN)
 
-#define P_ISREG(p) ((p) == 0)
+#define P_ISREG(p) ((p) == _REG)
 #define P_ISSUB(p) (((p) & _SUB) == _SUB)
 #define P_ISMOD(p) (((p) & _MOD) == _MOD)
 #define P_ISOWN(p) (((p) & _OWN) == _OWN)
 
-#define P_ALSUB(p) ((p) != 0)
-#define P_ALMOD(p) (((p) >> 1) != 0)
+#define P_ALSUB(p) ((p) != _REG)
+#define P_ALMOD(p) (((p) >> 1) != _REG)
