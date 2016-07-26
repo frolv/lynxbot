f96c039e3f8e974a08795b878271a671fbbf1037
Alexei Frolov <alexeifrolov3@gmail.com>
2016-07-26 13:43:05 -0400

    Add --virtual-hiscores option to cml

--- src/cmd/cml.cpp
+++ src/cmd/cml.cpp
@@ -3,16 +3,21 @@
 #include "../CommandHandler.h"
 #include "../OptionParser.h"
 
+#define NP 0
+#define SC 1
+#define VH 2
+
 /* full name of the command */
 CMDNAME("cml");
 /* description of the command */
 CMDDESCR("interact with crystalmathlabs trackers");
 /* command usage synopsis */
-CMDUSAGE("$cml [-s] [-nu] [RSN]");
+CMDUSAGE("$cml [-s|-v] [-nu] [RSN]");
 
 static const std::string CML_HOST = "https://crystalmathlabs.com";
 static const std::string CML_USER = "/tracker/track.php?player=";
 static const std::string CML_SCALC = "/tracker/suppliescalc.php";
+static const std::string CML_VHS = "/tracker/virtualhiscores.php?page=timeplayed";
 static const std::string CML_UPDATE = "/tracker/api.php?type=update&player=";
 
 static int updatecml(const std::string &rsn, std::string &err);
@@ -22,19 +27,22 @@ std::string CommandHandler::cml(struct cmdinfo *c)
 {
 	std::string output = "@" + c->nick + ", ";
 	std::string rsn, err;
-	bool usenick, update, scalc;
+	bool usenick, update;
+	int page;
 
-	OptionParser op(c->fullCmd, "nsu");
+	OptionParser op(c->fullCmd, "nsuv");
 	int opt;
 	static struct OptionParser::option long_opts[] = {
 		{ "help", NO_ARG, 'h' },
 		{ "nick", NO_ARG, 'n' },
+		{ "virtual-hiscores", NO_ARG, 'v' },
 		{ "suppliescalc", NO_ARG, 's' },
 		{ "update", NO_ARG, 'u' },
 		{ 0, 0, 0 }
 	};
 
-	usenick = update = scalc = false;
+	usenick = update = false;
+	page = NP;
 	while ((opt = op.getopt_long(long_opts)) != EOF) {
 		switch (opt) {
 		case 'h':
@@ -43,10 +51,13 @@ std::string CommandHandler::cml(struct cmdinfo *c)
 			usenick = true;
 			break;
 		case 's':
-			scalc = true;
+			page = SC;
 			break;
 		case 'u':
 			update = true;
+			break;
+		case 'v':
+			page = VH;
 			break;
 		case '?':
 			return std::string(op.opterr());
@@ -56,18 +67,19 @@ std::string CommandHandler::cml(struct cmdinfo *c)
 	}
 
 	if (op.optind() == c->fullCmd.length()) {
-		if (scalc) {
+		if (page) {
 			if (update || usenick)
 				return CMDNAME + ": cannot use other options "
-					"with -s";
-			return "[CML] " + CML_HOST + CML_SCALC;
+					"with " + (page == SC ? "-s" : "-v");
+			return "[CML] " + CML_HOST + (page == SC
+					? CML_SCALC : CML_VHS);
 		}
 		if (update)
 			return CMDNAME + ": must provide RSN to update";
 		if (usenick)
 			return CMDNAME + ": no Twitch nick given";
 		return "[CML] " + CML_HOST;
-	} else if (scalc) {
+	} else if (page) {
 		return USAGEMSG(CMDNAME, CMDUSAGE);
 	}
 
