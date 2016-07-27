f88eeedeff373a03cfb6ec72230242f2513e32f5
Alexei Frolov <alexeifrolov3@gmail.com>
2016-07-27 16:11:53 -0400

    Add latest release to $about

--- src/cmd/about.cpp
+++ src/cmd/about.cpp
@@ -3,32 +3,44 @@
 #include "../OptionParser.h"
 #include "../version.h"
 
+#define ABT 0
+#define REL 1
+#define SRC 2
+
 /* full name of the command */
 CMDNAME("about");
 /* description of the command */
 CMDDESCR("print bot information");
 /* command usage synopsis */
-CMDUSAGE("$about [-s]");
+CMDUSAGE("$about [-s|-r]");
 
 static const std::string SOURCE = "https://github.com/frolv/lynxbot";
+static const std::string RELEASE =
+	"https://github.com/frolv/lynxbot/releases/latest";
 
 /* about: print bot information */
 std::string CommandHandler::about(struct cmdinfo *c)
 {
-	int opt;
-	OptionParser op(c->fullCmd, "s");
+	int opt, type;
+	OptionParser op(c->fullCmd, "rs");
 	static struct OptionParser::option long_opts[] = {
 		{ "help", NO_ARG, 'h' },
+		{ "latest-release", NO_ARG, 'r' },
 		{ "source", NO_ARG, 's' },
 		{ 0, 0, 0 }
 	};
 
+	type = ABT;
 	while ((opt = op.getopt_long(long_opts)) != EOF) {
 		switch (opt) {
 		case 'h':
 			return HELPMSG(CMDNAME, CMDUSAGE, CMDDESCR);
+		case 'r':
+			type = REL;
+			break;
 		case 's':
-			return "[ABOUT] source: " + SOURCE;
+			type = SRC;
+			break;
 		case '?':
 			return std::string(op.opterr());
 		default:
@@ -39,7 +51,14 @@ std::string CommandHandler::about(struct cmdinfo *c)
 	if (op.optind() != c->fullCmd.length())
 		return USAGEMSG(CMDNAME, CMDUSAGE);
 
-	return "[ABOUT] " + m_name + " is running " + std::string(BOT_NAME)
-		+ " " + std::string(BOT_VERSION) + ". Find out more at "
-		+ std::string(BOT_WEBSITE);
+	switch (type) {
+	case REL:
+		return "[ABOUT] " + RELEASE;
+	case SRC:
+		return "[ABOUT] " + SOURCE;
+	default:
+		return "[ABOUT] " + m_name + " is running "
+			+ std::string(BOT_NAME) + " " + std::string(BOT_VERSION)
+			+ ". Find out more at " + std::string(BOT_WEBSITE);
+	}
 }
