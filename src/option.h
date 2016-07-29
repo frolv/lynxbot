cc1ebe79de26f5521971fde4fb02d8192079bdbf
Alexei Frolov <alexeifrolov3@gmail.com>
2016-07-29 17:18:50 -0400

    Add comments to option.h

--- src/option.h
+++ src/option.h
@@ -12,19 +12,33 @@
 #define NO_ARG  1
 #define REQ_ARG 2
 
+/* index in argv being parsed */
 extern int optind;
+
+/* argument provided to option */
 extern char *optarg;
 
+/* struct describing an option for the getopt_long function */
 struct option {
 	const char *name;
 	int type;
 	int val;
 };
 
+/* opt_init: reset all getopt variables */
 void opt_init();
+
+/* getopt: parse command options */
 int getopt(int argc, char **argv, const char *optstr);
+
+/*
+ * getopt_long: parse command options with suppport
+ * for long options starting with '--'
+ */
 int getopt_long(int argc, char **argv, const char *optstr,
 		const struct option *longopts);
+
+/* opterr: return string describing getopt error */
 char *opterr();
 
 #endif
