4ed5616d342a83dd8545f4e2d5730ddd2c756cce
Alexei Frolov <alexeifrolov3@gmail.com>
2016-07-29 21:24:25 -0400

    Updated addrec function to new format

--- src/cmd/addrec.cpp
+++ src/cmd/addrec.cpp
@@ -1,67 +1,93 @@
+#include <string.h>
 #include "command.h"
 #include "../CommandHandler.h"
-#include "../OptionParser.h"
+#include "../option.h"
+#include "../stringparse.h"
 
 /* full name of the command */
-CMDNAME("addrec");
+_CMDNAME("addrec");
 /* description of the command */
-CMDDESCR("add a recurring message");
+_CMDDESCR("add a recurring message");
 /* command usage synopsis */
-CMDUSAGE("$addrec [-c INT] MSG");
+_CMDUSAGE("$addrec [-c INT] MSG");
 
 /* addrec: add a recurring message */
 std::string CommandHandler::addrec(char *out, struct command *c)
 {
-	if (!P_ALMOD(c->privileges)) {
-		PERM_DENIED(out, c->nick, c->argv[0]);
-		return "";
-	}
+	time_t cooldown;
+	char msg[MAX_MSG];
 
-	std::string output = "@" + std::string(c->nick) + ", ";
-	time_t cooldown = 300;
 	int opt;
-	OptionParser op(c->fullCmd, "c:");
-	static struct OptionParser::option long_opts[] = {
+	static struct option long_opts[] = {
 		{ "cooldown", REQ_ARG, 'c' },
 		{ "help", NO_ARG, 'h' },
 		{ "interval", REQ_ARG, 'c' },
 		{ 0, 0, 0 }
 	};
 
-	while ((opt = op.getopt_long(long_opts)) != EOF) {
+	if (!P_ALMOD(c->privileges)) {
+		PERM_DENIED(out, c->nick, c->argv[0]);
+		return "";
+	}
+
+	opt_init();
+	cooldown = 300;
+	while ((opt = getopt_long(c->argc, c->argv, "c:", long_opts)) != EOF) {
 		switch (opt) {
 		case 'c':
-			try {
-				cooldown = 60 * std::stoi(op.optarg());
-			} catch (std::invalid_argument) {
-				return CMDNAME + ": invalid number -- '"
-					+ op.optarg() + "'";
-			} catch (std::out_of_range) {
-				return CMDNAME + ": number too large";
+			/* user provided a cooldown */
+			if (!parsenum(optarg, &cooldown)) {
+				_sprintf(out, MAX_MSG, "%s: invalid number: %s",
+						c->argv[0], optarg);
+				return "";
 			}
+			if (cooldown < 0) {
+				_sprintf(out, MAX_MSG, "%s: cooldown cannot be "
+						"negative", c->argv[0]);
+				return "";
+			}
+			cooldown *= 60;
 			break;
 		case 'h':
-			return HELPMSG(CMDNAME, CMDUSAGE, CMDDESCR);
+			_HELPMSG(out, _CMDNAME, _CMDUSAGE, _CMDDESCR);
+			return "";
 		case '?':
-			return std::string(op.opterr());
+			_sprintf(out, MAX_MSG, "%s", opterr());
+			return "";
 		default:
 			return "";
 		}
 	}
 
-	if (cooldown % 300 != 0)
-		return CMDNAME + ": interval must be a multiple of 5 mins";
-	else if (cooldown > 3600)
-		return CMDNAME + ": interval cannot be longer than 60 mins";
+	if (cooldown % 300 != 0) {
+		_sprintf(out, MAX_MSG, "%s: interval must be a multiple "
+				"of 5 mins", c->argv[0]);
+		return "";
+	}
+	else if (cooldown > 3600) {
+		_sprintf(out, MAX_MSG, "%s: interval cannot be longer "
+				"than 60 mins", c->argv[0]);
+		return "";
+	}
+	if (optind == c->argc) {
+		_USAGEMSG(out, _CMDNAME, _CMDUSAGE);
+		return "";
+	}
 
-	if (op.optind() == c->fullCmd.length())
-		return USAGEMSG(CMDNAME, CMDUSAGE);
-	else if (!m_evtp->addMessage(c->fullCmd.substr(op.optind()), cooldown))
-		return CMDNAME + ": limit of 5 recurring messages reached";
+	/* build the message */
+	msg[0] = '\0';
+	for (; optind < c->argc; ++optind) {
+		strcat(msg, c->argv[optind]);
+		if (optind != c->argc - 1)
+			strcat(msg, " ");
+	}
+
+	if (!m_evtp->addMessage(msg, cooldown))
+		_sprintf(out, MAX_MSG, "%s: limit of 5 recurring "
+				"messages reached", c->argv[0]);
 	else
-		output += "recurring message \""
-			+ c->fullCmd.substr(op.optind())
-			+ "\" has been added at a "
-			+ std::to_string(cooldown / 60) + " min interval.";
-	return output;
+		_sprintf(out, MAX_MSG, "@%s, recurring message \"%s\" has been "
+				"added at a %ld min interval", c->nick, msg,
+				cooldown / 60);
+	return "";
 }
