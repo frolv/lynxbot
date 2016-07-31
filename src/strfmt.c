d39dd7bb5148b0570de95d8dcb8ba34f60c198f7
Alexei Frolov <alexeifrolov3@gmail.com>
2016-07-30 23:12:56 -0400

    Add strfmt.{c,h}

---
+++ src/strfmt.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "strfmt.h"
+#include <stdio.h>
+
+/* fmtnum: format a number with commas */
+void fmtnum(char *out, size_t size, const char *num)
+{
+	const char *s;
+	size_t i, sz;
+
+	if (size == 0)
+		return;
+
+	i = sz = 0;
+	if (*num == '-') {
+		*out++ = *num++;
+		++sz;
+	}
+	while (num[i] && num[i] != '.')
+		++i;
+	s = num;
+	while (*s && *s != '.' && sz < size - 1) {
+		*out++ = *s++;
+		++sz;
+		if (--i && i % 3 == 0 && sz < size - 1) {
+			*out++ = ',';
+			++sz;
+		}
+	}
+	if (*s == '.')
+		strncat(out, s, size - sz);
+	else
+		*out = '\0';
+}
---
+++ src/strfmt.h
@@ -0,0 +1,11 @@
+/*
+ * strfmt.h: functions to format strings
+ */
+
+#ifndef _STRFMT_H
+#define _STRFMT_H
+
+/* fmtnum: format a number with commas */
+void fmtnum(char *out, size_t size, const char *num);
+
+#endif
