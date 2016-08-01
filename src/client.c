86651f99e8d587acb582cca53eeb699a40b5cf03
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-01 08:34:22 -0400

    Change client to C

---
+++ src/client.c
@@ -0,0 +1,162 @@
+#ifdef __linux__
+# include <netdb.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <netinet/in.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+#endif
+
+#ifdef _WIN32
+# include <sdkddkver.h>
+# include <WS2tcpip.h>
+#endif
+
+#include <stdio.h>
+#include "client.h"
+
+#ifdef __linux__
+static int connect_unix(struct client *cl, const char *serv, const char *port);
+#endif
+#ifdef _WIN32
+static int connect_win(struct client *cl, const char *serv, const char *port);
+#endif
+
+/* cconnect: connect cl to server serv on port port */
+int cconnect(struct client *cl, const char *serv, const char *port)
+{
+#ifdef __linux__
+	return connect_unix(cl, serv, port);
+#endif
+#ifdef _WIN32
+	return connect_win(cl, serv, port);
+#endif
+}
+
+/* cdisconnect: disconnect cl from current server */
+int cdisconnect(struct client *cl)
+{
+	if (cl->connected) {
+		cl->connected = false;
+#ifdef __linux__
+		if (close(cl->fd) == -1) {
+			perror("close");
+			return 1;
+		}
+#endif
+#ifdef _WIN32
+		closesocket(cl->sock);
+		WSACleanup();
+#endif
+	}
+	return 0;
+}
+
+/* cwrite: write data to cl server */
+int cwrite(struct client *cl, const char *msg)
+{
+	if (!cl->connected)
+		return -1;
+#ifdef __linux__
+	return write(cl->fd, msg, strlen(msg));
+#endif
+#ifdef _WIN32
+	return send(cl->sock, msg, strlen(msg), NULL);
+#endif
+}
+
+/* cread: read data from cl server */
+int cread(struct client *cl, char *buf, size_t sz)
+{
+	int bytes;
+
+	if (!cl->connected)
+		return -1;
+
+#ifdef __linux__
+	bytes = read(cl->fd, buf, sz - 1);
+#endif
+#ifdef _WIN32
+	bytes = recv(cl->sock, buf, sz - 1, 0);
+#endif
+	if (bytes >= 0)
+		buf[bytes] = '\0';
+	return bytes;
+}
+
+#ifdef __linux__
+static int connect_unix(struct client *cl, const char *serv, const char *port)
+{
+	struct hostent *hp;
+	struct sockaddr_in s;
+
+	cl->connected = 0;
+	if (!(hp = gethostbyname(serv))) {
+		fprintf(stderr, "could not find host %s\n", serv);
+		return 1;
+	}
+
+	if ((cl->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return 1;
+	}
+
+	memset(&s, '\0', sizeof(s));
+	s.sin_family = AF_INET;
+	memcpy(&s.sin_addr, hp->h_addr_list[0], hp->h_length);
+	s.sin_port = htons(atoi(port));
+
+	if (connect(cl->fd, (struct sockaddr *)&s, sizeof(s)) < 0) {
+		perror("connect");
+		return 1;
+	}
+	cl->connected = 1;
+	return 0;
+}
+#endif
+
+#ifdef _WIN32
+static int connect_win(struct client *cl, const char *serv, const char *port)
+{
+	struct addrinfo hints, *servinfo;
+	int error;
+
+	cl->connected = 0;
+	if ((error = WSAStartup(MAKEWORD(2, 2), cl->wsa))) {
+		fprintf(stderr, "WSAStartup failed. Error %d: %s\n",
+				error, WSAGetLastError());
+		return 1;
+	}
+
+	memset(&hints, '\0', sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	if ((error = getaddrinfo(serv, port, &hints, &servinfo))) {
+		fprintf(stderr, "getaddrinfo failed. Error %d: %s\n",
+				error, WSAGetLastError());
+		return 1;
+	}
+
+	if ((cl->sock = socket(servinfo->ai_family, servinfo->ai_socktype,
+		servinfo->ai_protocol)) == INVALID_SOCKET) {
+		fprintf(stderr, "Could not open socket. Error: %s\n",
+				WSAGetLastError());
+		return 1;
+	}
+
+	if (connect(cl->sock, servinfo->ai_addr, servinfo->ai_addrlen)
+		== SOCKET_ERROR) {
+		fprintf(stderr, "Could not connect to socket. Error: %s\n",
+				WSAGetLastError());
+		closesocket(cl->sock);
+		return 1;
+	}
+
+	freeaddrinfo(servinfo);
+	cl->connected = 1;
+	return 0;
+}
+#endif
--- src/client.h
+++ src/client.h
@@ -1,30 +1,37 @@
-#pragma once
+/*
+ * client.h: definition of client struct and functions to use client
+ * for connecting, sending and recieving data through a socket
+ */
 
-#include <iostream>
-#include <string>
+#ifndef _CLIENT_H
+#define _CLIENT_H
 
 #ifdef _WIN32
 # include <Winsock2.h>
 #endif
 
-class Client {
-	public:
-		Client(const char *serv, const char *port);
-		~Client();
-		bool cconnect();
-		void cdisconnect();
-		int32_t cwrite(const char *msg);
-		int32_t cread(char *buf, size_t sz);
-	private:
-		const char *m_serv, *m_port;
-		bool m_connected;
+/* a simple networking client */
+struct client {
+	int connected;
 #ifdef __linux__
-		int m_fd;
-		bool connect_unix();
+	int fd;
 #endif
 #ifdef _WIN32
-		SOCKET m_socket;
-		WSADATA m_wsa;
-		bool connect_win();
+	SOCKET sock;
+	WSADATA wsa;
 #endif
 };
+
+/* cconnect: connect cl to server serv on port port */
+int cconnect(struct client *cl, const char *serv, const char *port);
+
+/* cdisconnect: disconnect cl from current server */
+int cdisconnect(struct client *cl);
+
+/* cwrite: write data to cl server */
+int cwrite(struct client *cl, const char *msg);
+
+/* cread: read data from cl server */
+int cread(struct client *cl, char *buf, size_t sz);
+
+#endif
