a1a43f3911c978dface0af9448f35baeaa83faf2
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-02 18:46:30 -0400

    setrec: update format

--- src/cmd/setrec.cpp
+++ src/cmd/setrec.cpp
@@ -1,54 +1,55 @@
+#include <string.h>
 #include "command.h"
 #include "../CommandHandler.h"
-#include "../OptionParser.h"
+#include "../option.h"
 
 /* full name of the command */
-CMDNAME("setrec");
+_CMDNAME("setrec");
 /* description of the command */
-CMDDESCR("enable/disabe recurring messages");
+_CMDDESCR("enable/disabe recurring messages");
 /* command usage synopsis */
-CMDUSAGE("$setrec on|off");
+_CMDUSAGE("$setrec on|off");
 
 /* setrec: enable and disable recurring messages */
 std::string CommandHandler::setrec(char *out, struct command *c)
 {
+	int opt;
+	static struct option long_opts[] = {
+		{ "help", NO_ARG, 'h' },
+		{ 0, 0, 0 }
+	};
+
 	if (!P_ALMOD(c->privileges)) {
 		PERM_DENIED(out, c->nick, c->argv[0]);
 		return "";
 	}
 
-	std::string outp, set;
-
-	int opt;
-	OptionParser op(c->fullCmd, "");
-	static struct OptionParser::option long_opts[] = {
-		{ "help", NO_ARG, 'h' },
-		{ 0, 0, 0 }
-	};
-
-	while ((opt = op.getopt_long(long_opts)) != EOF) {
+	opt_init();
+	while ((opt = getopt_long(c->argc, c->argv, "", long_opts)) != EOF) {
 		switch (opt) {
 		case 'h':
-			return HELPMSG(CMDNAME, CMDUSAGE, CMDDESCR);
+			_HELPMSG(out, _CMDNAME, _CMDUSAGE, _CMDDESCR);
+			return "";
 		case '?':
-			return std::string(op.opterr());
+			_sprintf(out, MAX_MSG, "%s", opterr());
+			return "";
 		default:
 			return "";
 		}
 	}
 
-	outp = "@" + std::string(c->nick) + ", ";
-	if (op.optind() == c->fullCmd.length()
-			|| ((set = c->fullCmd.substr(op.optind())) != "on"
-			&& set != "off"))
-		return USAGEMSG(CMDNAME, CMDUSAGE);
-
-	if (set == "on") {
+	if (optind != c->argc - 1) {
+		_USAGEMSG(out, _CMDNAME, _CMDUSAGE);
+	} else if (strcmp(c->argv[optind], "on") == 0) {
 		m_evtp->activateMessages();
-		outp += "recurring messages enabled.";
-	} else {
+		_sprintf(out, MAX_MSG, "@%s, recurring mesasges enabled.",
+				c->nick);
+	} else if (strcmp(c->argv[optind], "off") == 0) {
 		m_evtp->deactivateMessages();
-		outp += "recurring messages disabled.";
+		_sprintf(out, MAX_MSG, "@%s, recurring mesasges disabled.",
+				c->nick);
+	} else {
+		_USAGEMSG(out, _CMDNAME, _CMDUSAGE);
 	}
-	return outp;
+	return "";
 }
