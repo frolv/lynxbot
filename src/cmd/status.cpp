a7b7e6baecf1d8e3b7342514af83987f035677f0
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-02 21:08:18 -0400

    status: update command format

--- src/cmd/status.cpp
+++ src/cmd/status.cpp
@@ -4,129 +4,148 @@
 #include <utils.h>
 #include "command.h"
 #include "../CommandHandler.h"
-#include "../OptionParser.h"
+#include "../option.h"
+
+#define MAX_URL 128
 
 /* full name of the command */
-CMDNAME("status");
+_CMDNAME("status");
 /* description of the command */
-CMDDESCR("set channel status");
+_CMDDESCR("set channel status");
 /* command usage synopsis */
-CMDUSAGE("status [-a] [STATUS]");
+_CMDUSAGE("status [-a] [STATUS]");
 
-static const std::string TWITCH_API = "https://api.twitch.tv/kraken/channels/";
+static const char *TWITCH_API = "https://api.twitch.tv/kraken/channels/";
 
-static std::string curr_status(const std::string &channel, cpr::Header &head,
-		std::string &err);
-static std::string set_status(const std::string &channel,
-		const std::string &status, cpr::Header &head,
-		std::string &err);
+static int curr_status(char *buf, const char *channel, const cpr::Header *head);
+static void set_status(char *out, const char *channel, const char *status,
+		const cpr::Header *head);
 
 /* status: set channel status */
 std::string CommandHandler::status(char *out, struct command *c)
 {
-	if (!P_ALMOD(c->privileges)) {
-		PERM_DENIED(out, c->nick, c->argv[0]);
-		return "";
-	}
-
-	std::string output, status, err;
-	bool append;
-	cpr::Header head{{ "Accept", "application/vnd.twitchtv.v3+json" },
+	const cpr::Header head{{ "Accept", "application/vnd.twitchtv.v3+json" },
 		{ "Authorization", "OAuth " + std::string(m_token) }};
 
+	char buf[MAX_MSG];
+	int append;
+
 	int opt;
-	OptionParser op(c->fullCmd, "a");
-	static struct OptionParser::option long_opts[] = {
+	static struct option long_opts[] = {
 		{ "append", NO_ARG, 'a' },
 		{ "help", NO_ARG, 'h' },
 		{ 0, 0, 0 }
 	};
 
-	append = false;
-	while ((opt = op.getopt_long(long_opts)) != EOF) {
+	if (!P_ALMOD(c->privileges)) {
+		PERM_DENIED(out, c->nick, c->argv[0]);
+		return "";
+	}
+
+	opt_init();
+	append = 0;
+	buf[0] = '\0';
+	while ((opt = getopt_long(c->argc, c->argv, "a", long_opts)) != EOF) {
 		switch (opt) {
 		case 'a':
-			append = true;
+			append = 1;
 			break;
 		case 'h':
-			return HELPMSG(CMDNAME, CMDUSAGE, CMDDESCR);
+			_HELPMSG(out, _CMDNAME, _CMDUSAGE, _CMDDESCR);
+			return "";
 		case '?':
-			return std::string(op.opterr());
+			_sprintf(out, MAX_MSG, "%s", opterr());
+			return "";
 		default:
 			return "";
 		}
 	}
 
 	/* get the current status if no arg provided or if appending */
-	if (op.optind() == c->fullCmd.length() || append) {
-		if ((status = curr_status(m_channel, head, err)).empty())
-			return CMDNAME + ": " + err;
+	if (optind == c->argc || append) {
+		if (!curr_status(buf, m_channel, &head)) {
+			_sprintf(out, MAX_MSG, "%s: %s", c->argv[0], buf);
+			return "";
+		}
 	}
 
-	if (op.optind() == c->fullCmd.length()) {
+	if (optind == c->argc) {
 		if (append)
-			return c->cmd + ": no text to append";
-		return "[STATUS] Current status for " + std::string(m_channel)
-			+ " is \"" + status + "\".";
+			_sprintf(out, MAX_MSG, "%s: no text to append",
+					c->argv[0]);
+		else
+			_sprintf(out, MAX_MSG, "[STATUS] Current status for %s "
+					"is \"%s\".", m_channel, buf);
+		return "";
 	}
-	if (append)
-		status += " ";
-	status += c->fullCmd.substr(op.optind());
 
-	if ((output = set_status(m_channel, status, head, err)).empty())
-		return CMDNAME + ": " + err;
-	return output;
+	if (append)
+		strcat(buf, " ");
+	argvcat(buf + strlen(buf), c->argc, c->argv, optind, 1);
+	set_status(out, m_channel, buf, &head);
+
+	return "";
 }
 
 /* curr_status: get current status of channel */
-static std::string curr_status(const std::string &channel, cpr::Header &head,
-		std::string &err)
+static int curr_status(char *buf, const char *channel, const cpr::Header *head)
 {
-	std::string out;
 	cpr::Response resp;
 	Json::Reader reader;
 	Json::Value response;
+	char url[MAX_URL];
+	char *s;
 
-	resp = cpr::Get(cpr::Url(TWITCH_API + channel), head);
+	_sprintf(url, MAX_URL, "%s%s", TWITCH_API, channel);
+	resp = cpr::Get(cpr::Url(url), *head);
+
 	if (reader.parse(resp.text, response)) {
 		if (response.isMember("error")) {
-			err = response["error"].asString();
-			std::transform(err.begin(), err.end(),
-					err.begin(), tolower);
-			return "";
+			_sprintf(buf, MAX_MSG, "%s",
+					response["error"].asCString());
+			for (s = buf; *s; ++s)
+				*s = tolower(*s);
+			return 0;
 		}
-		return response["status"].asString();
+		_sprintf(buf, MAX_MSG, "%s", response["status"].asCString());
+		return 1;
 	}
-	err = "could not parse channel data";
-	return "";
+	_sprintf(buf, MAX_MSG, "could not parse channel data");
+	return 0;
 }
 
 /* curr_status: set new status of channel */
-static std::string set_status(const std::string &channel,
-		const std::string &status, cpr::Header &head,
-		std::string &err)
+static void set_status(char *out, const char *channel, const char *status,
+		const cpr::Header *head)
 {
-	std::string out, content;
 	cpr::Response resp;
 	Json::Reader reader;
 	Json::Value response;
+	char body[MAX_MSG];
+	char url[MAX_URL];
+	char *s;
 
-	out = tw::pencode(status, " ");
-	std::replace(out.begin(), out.end(), ' ', '+');
-	content = "channel[status]=" + out;
+	_sprintf(body, MAX_MSG, "channel[status]=%s",
+			tw::pencode(status, " ").c_str());
+	for (s = body; *s; ++s) {
+		if (*s == ' ')
+			*s = '+';
+	}
 
-	resp = cpr::Put(cpr::Url(TWITCH_API + channel), cpr::Body(content),
-			head);
+	_sprintf(url, MAX_URL, "%s%s", TWITCH_API, channel);
+	resp = cpr::Put(cpr::Url(url), cpr::Body(body), *head);
+
 	if (reader.parse(resp.text, response)) {
 		if (response.isMember("error")) {
-			err = response["error"].asString();
-			std::transform(err.begin(), err.end(),
-					err.begin(), tolower);
-			return "";
+			_sprintf(out, MAX_MSG, "%s: %s", _CMDNAME,
+					response["error"].asCString());
+			for (s = out; *s; ++s)
+				*s = tolower(*s);
+			return;
 		}
-		return "[STATUS] Channel status changed to \""
-			+ response["status"].asString() + "\".";
+		_sprintf(out, MAX_MSG, "[STATUS] Channel status changed to "
+				"\"%s\".", response["status"].asCString());
+		return;
 	}
-	err = "could not parse channel data";
-	return "";
+	_sprintf(out, MAX_MSG, "could not parse channel data");
 }
