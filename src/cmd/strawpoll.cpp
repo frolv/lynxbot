d6d048fba981ae369480ec29bebc57c28d908bb1
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-02 23:20:16 -0400

    strawpoll: update command format

--- src/cmd/strawpoll.cpp
+++ src/cmd/strawpoll.cpp
@@ -3,36 +3,32 @@
 #include <utils.h>
 #include "command.h"
 #include "../CommandHandler.h"
-#include "../OptionParser.h"
+#include "../option.h"
 
 /* full name of the command */
-CMDNAME("strawpoll");
+_CMDNAME("strawpoll");
 /* description of the command */
-CMDDESCR("create polls");
+_CMDDESCR("create polls");
 /* command usage synopsis */
-CMDUSAGE("$strawpoll [-bcm] QUESTION | OPTION1 | OPTION2...");
+_CMDUSAGE("$strawpoll [-bcm] QUESTION | OPTION1 | OPTION2...");
 
-static const std::string STRAWPOLL_HOST = "https://strawpoll.me";
-static const std::string STRAWPOLL_API = "/api/v2/polls";
-static bool binary, captcha, multi;
+static const char *STRAWPOLL_HOST = "https://strawpoll.me";
+static const char *STRAWPOLL_API = "/api/v2/polls";
 
-static std::string create_poll(const std::vector<std::string> &tokens,
-		std::string &err);
+/* create a yes/no poll */
+static bool binary;
+/* whether to use captcha */
+static bool captcha;
+/* whether to allow multiple choices */
+static bool multi;
+
+static void create_poll(char *out, char *pollbuf, struct command *c);
 
 /* strawpoll: create polls */
 std::string CommandHandler::strawpoll(char *out, struct command *c)
 {
-	if (!P_ALMOD(c->privileges)) {
-		PERM_DENIED(out, c->nick, c->argv[0]);
-		return "";
-	}
-
-	std::string err;
-	std::vector<std::string> tokens;
-
 	int opt;
-	OptionParser op(c->fullCmd, "bcm");
-	static struct OptionParser::option long_opts[] = {
+	static struct option long_opts[] = {
 		{ "binary", NO_ARG, 'b' },
 		{ "captcha", NO_ARG, 'c' },
 		{ "help", NO_ARG, 'h' },
@@ -40,8 +36,14 @@ std::string CommandHandler::strawpoll(char *out, struct command *c)
 		{ 0, 0, 0 }
 	};
 
+	if (!P_ALMOD(c->privileges)) {
+		PERM_DENIED(out, c->nick, c->argv[0]);
+		return "";
+	}
+
+	opt_init();
 	binary = captcha = multi = false;
-	while ((opt = op.getopt_long(long_opts)) != EOF) {
+	while ((opt = getopt_long(c->argc, c->argv, "bcm", long_opts)) != EOF) {
 		switch (opt) {
 		case 'b':
 			binary = true;
@@ -50,78 +52,110 @@ std::string CommandHandler::strawpoll(char *out, struct command *c)
 			captcha = true;
 			break;
 		case 'h':
-			return HELPMSG(CMDNAME, CMDUSAGE, CMDDESCR);
+			_HELPMSG(out, _CMDNAME, _CMDUSAGE, _CMDDESCR);
+			return "";
 		case 'm':
 			multi = true;
 			break;
 		case '?':
-			return std::string(op.opterr());
+			_sprintf(out, MAX_MSG, "%s", opterr());
+			return "";
 		default:
 			return "";
 		}
 	}
 
-	if (op.optind() == c->fullCmd.length())
-		return USAGEMSG(CMDNAME, CMDUSAGE);
-
-	utils::split(c->fullCmd.substr(op.optind()), '|', tokens);
-	if (binary && tokens.size() != 1)
-		return c->cmd + ": cannot provide options for binary poll";
-	if (!binary && tokens.size() < 3)
-		return c->cmd + ": poll must have a question and at least "
-			"two answers";
-
-	std::string poll;
-	if ((poll = create_poll(tokens, err)).empty()) {
-		m_poll[0] = '\0';
-		return c->cmd + ": " + err;
+	if (optind == c->argc) {
+		_USAGEMSG(out, _CMDNAME, _CMDUSAGE);
+		return "";
 	}
-	strcpy(m_poll, poll.c_str());
-	return "[STRAWPOLL] " + STRAWPOLL_HOST + "/" + poll;
+
+	create_poll(out, m_poll, c);
+	/* if (binary && tokens.size() != 1) */
+	/* 	return c->cmd + ": cannot provide options for binary poll"; */
+	/* if (!binary && tokens.size() < 3) */
+	/* 	return c->cmd + ": poll must have a question and at least " */
+	/* 		"two answers"; */
+
+	/* std::string poll; */
+	/* if ((poll = create_poll(tokens, err)).empty()) { */
+	/* 	m_poll[0] = '\0'; */
+	/* 	return c->cmd + ": " + err; */
+	/* } */
+	/* strcpy(m_poll, poll.c_str()); */
+	/* return "[STRAWPOLL] " + STRAWPOLL_HOST + "/" + poll; */
+	return "";
 }
 
 /* create_poll: create a strawpoll, return id */
-static std::string create_poll(const std::vector<std::string> &tokens,
-		std::string &err)
+static void create_poll(char *out, char *pollbuf, struct command *c)
 {
 	/* json values to hold created poll, poll options and http response */
 	Json::Value poll, options(Json::arrayValue), response;
 	Json::FastWriter fw;
 	Json::Reader reader;
 	cpr::Response resp;
-	std::string content;
-	size_t i;
+	char buf[MAX_MSG];
+	char *quest, *s, *t;
+
+	argvcat(buf, c->argc, c->argv, optind, 1);
+	quest = buf;
+	if (!(s = strchr(quest, '|'))) {
+		if (!binary) {
+			_sprintf(out, MAX_MSG, "%s: poll must have a question "
+					"and at least two answers", c->argv[0]);
+			return;
+		}
+	} else {
+		*s = '\0';
+		if (binary) {
+			_sprintf(out, MAX_MSG, "%s: cannot provide answers "
+					"for binary poll", c->argv[0]);
+			return;
+		}
+	}
 
 	if (binary) {
 		options.append("yes");
 		options.append("no");
 	} else {
-		for (i = 1; i < tokens.size(); ++i)
-			options.append(tokens[i]);
+		for (++s; *s && (t = strchr(s, '|')); s = t + 1) {
+			*t = '\0';
+			options.append(s);
+		}
+		if (*s)
+			options.append(s);
+	}
+
+	if (options.size() < 2) {
+		_sprintf(out, MAX_MSG, "%s: poll must have a question "
+				"and at least two answers", c->argv[0]);
+		return;
 	}
 
 	/* populate the poll json */
-	poll["title"] = tokens[0];
+	poll["title"] = quest;
 	poll["options"] = options;
 	poll["captcha"] = captcha;
 	poll["multi"] = multi;
 
 	/* format and post the poll */
-	content = fw.write(poll);
-	resp = cpr::Post(cpr::Url(STRAWPOLL_HOST + STRAWPOLL_API),
-			cpr::Body(content), cpr::Header{
+	_sprintf(buf, MAX_MSG, "%s%s", STRAWPOLL_HOST, STRAWPOLL_API);
+	resp = cpr::Post(cpr::Url(buf), cpr::Body(fw.write(poll)), cpr::Header{
 			{ "Connection", "close" },
-			{ "Content-Type", "application/json" },
-			{ "Content-Length", std::to_string(content.length())}});
+			{ "Content-Type", "application/json" }});
 
 	/* read id from response */
 	if (reader.parse(resp.text, response)) {
 		if (!response.isMember("id")) {
-			err = "poll could not be created";
-			return "";
+			_sprintf(out, MAX_MSG, "%s: poll could not be created",
+					c->argv[0]);
+			return;
 		}
-		return response["id"].asString();
+		_sprintf(pollbuf, MAX_LEN, "%d", response["id"].asInt());
+		_sprintf(out, MAX_MSG, "[STRAWPOLL] %s/%s",
+				STRAWPOLL_HOST, pollbuf);
+		return;
 	}
-	err = "could not parse response";
-	return "";
+	_sprintf(out, MAX_MSG, "%s: could not parse response", c->argv[0]);
 }
