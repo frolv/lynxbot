e76b1b5568f1d378a8091b8d3a155967f756b95a
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-06 12:12:25 -0400

    add struct sedinfo and create parse function

--- src/sed.cpp
+++ src/sed.cpp
@@ -3,44 +3,52 @@
 #include "sed.h"
 #include "string.h"
 
+static struct sedinfo {
+	const char *regex;
+	const char *replace;
+	int global;
+	int ignore;
+	int error;
+	int errtok;
+}
+
+static int parsecmd(struct sedinfo *s, char *cmd);
+static void puterr(char *out, struct sedinfo *s);
+
 int sed(char *s, size_t max, const char *input, const char *sedcmd)
 {
 	char cmd[MAX_MSG];
-	char *t, *regex, *replace;
-	int delim, global, ign;
+	char *t;
+	struct sedinfo sedbuf;
 
 	strncpy(cmd, sedcmd, MAX_MSG);
-	if (cmd[0] != 's') {
-		_sprintf(s, max, "sed: invalid command '%c'", cmd[0]);
+	if (!parsecmd(&sedbuf, cmd)) {
+		puterr(out, &sedbuf);
 		return 0;
 	}
-	if (!(delim = cmd[1]) || !*(regex = cmd + 2)) {
-		_sprintf(s, max, "sed: unterminated 's' command");
-		return 0;
-	}
-	if (!(t = strchr(regex, delim)) || !*(replace = t + 1)) {
-		_sprintf(s, max, "sed: unterminated 's' command");
-		return 0;
-	}
-	*t = '\0';
-	if (!(t = strchr(replace, delim))) {
-		_sprintf(s, max, "sed: unterminated 's' command");
-		return 0;
-	}
-	*t = '\0';
-	for (++t; *t; ++t) {
-		switch (*t) {
-		case 'g':
-			global = 1;
-			break;
-		case 'i':
-			ign = 1;
-			break;
-		default:
-			_sprintf(s, max, "sed: unknown option to 's': '%c'", *t);
-			return 0;
-		}
-	}
-	printf("%s\n%s\n%s\n", input, regex, replace);
+
+	printf("%s\n%s\n%s\n", input, sedbuf.regex, sedbuf.replace);
 	return 1;
+}
+
+#define INVCMD 1
+
+static int parsecmd(struct sedinfo *s, char *cmd)
+{
+	if (cmd[0] != 's') {
+		s->error = INVCMD;
+		s->errtok = cmd[0];
+		return 0;
+	}
+}
+
+static void puterr(char *out, struct sedinfo *s)
+{
+	switch (s->error) {
+	case INVCMD:
+		_sprintf(out, max, "sed: invalid command '%c'", s->errtok);
+		break;
+	default:
+		break;
+	}
 }
