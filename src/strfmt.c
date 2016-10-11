b8a1215f6064ac2ad3fb9ac6ccc4b4139c09d4d3
Alexei Frolov <alexeifrolov3@gmail.com>
2016-10-11 09:00:52 -0400

    strfmt function for formatting strings with format sequences

--- src/strfmt.c
+++ src/strfmt.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "strfmt.h"
+
+static const struct format *find_format(const struct format *fmt, int c);
 
 /* fmtnum: format a number with commas */
 void fmtnum(char *out, size_t size, const char *num)
@@ -30,4 +34,58 @@ void fmtnum(char *out, size_t size, const char *num)
 		strncat(out, s, size - sz);
 	else
 		*out = '\0';
+}
+
+char *strfmt(char *out, size_t size, const char *str,
+			const struct format *fmtchars)
+{
+	char *start, *s, *in;
+	size_t i, len;
+	const struct format *f;
+
+	start = out;
+	s = start;
+	i = 0;
+
+	for (; *str && i < size - 1; ++str) {
+		if (*str == '%') {
+			if (!str[1])
+				return NULL;
+
+			if (str[1] == '%') {
+				*s++ = *str++;
+				continue;
+			}
+
+			if (!fmtchars || !(f = find_format(fmtchars, str[1])))
+				return NULL;
+			if (!(in = f->fmtfun(f->data)))
+				return NULL;
+
+			snprintf(s, size - i, "%s", in);
+			len = strlen(in);
+			i += len;
+			s += len;
+
+			/* skip format char */
+			++str;
+
+			free(in);
+			continue;
+		}
+		*s++ = *str;
+		++i;
+	}
+	*s = '\0';
+
+	return start;
+}
+
+static const struct format *find_format(const struct format *fmt, int c)
+{
+	for (; fmt->val && fmt->data && fmt->fmtfun; ++fmt) {
+		if (fmt->val == c)
+			return fmt;
+	}
+	return NULL;
 }
--- src/strfmt.h
+++ src/strfmt.h
@@ -5,18 +5,31 @@
 #ifndef STRFMT_H
 #define STRFMT_H
 
+#define EFMT 1
+#define EEOL 2
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*
+ * A format struct defines a format sequence.
+ * val is the format character.
+ * fmtfun is a pointer to a function taking data as an argument and returning
+ * a *dynamically allocated* string representing data. This is the string that
+ * is inserted in place of the format sequence.
+ */
 struct format {
-	int	val;
-	void	*data;
-	char	(*fmtfun)(void *);
+	int		val;
+	const void	*data;
+	char		*(*fmtfun)(const void *);
 };
 
 /* fmtnum: format a number with commas */
 void fmtnum(char *out, size_t size, const char *num);
+
+char *strfmt(char *out, size_t size, const char *str,
+			const struct format *fmtchars);
 
 #ifdef __cplusplus
 } /* extern "C" */
